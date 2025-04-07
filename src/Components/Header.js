47d2b62a6eea52bc46ddf84534ea8a1a90be624f
senalfernando555 <senal3fernando@gmail.com>
2025-04-07 13:36:28 +0100

    Design of Nav-Bar Completed

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,17 +1,19 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./Header.css"; // Assuming you have a CSS file for styling
 
 function Header() {
   return (
     <header>
       <nav>
-        <ul>
+        <ul id="Primary">
           <li><Link to="/">Home</Link></li>
           <li><Link to="/register-drug">Register Drug</Link></li>
-          <li><Link to="/transfer-ownership">Transfer Ownership</Link></li>
+          {/* <li><Link to="/transfer-ownership">Transfer Ownership</Link></li> */}
           <li><Link to="/verify-drug">Verify Drug</Link></li>
           <li><Link to="/drug-details">Drug Details</Link></li>
           <li><Link to="/mark-counterfit">Mark Counterfit</Link></li>
+          <li><Link to="/mark-counterfit">Login/Register</Link></li>
         </ul>
       </nav>
     </header>
